f2dbe7ac89acb5b93f6eb26b443d166216cf7958
Pedro Lucas <pedro.a.kruger@gmail.com>
2025-02-26 13:58:59 -0300

    add infos

--- IAeML.py
+++ IAeML.py
@@ -1,1 +1,9 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+abre_arquivo_csv_com_pandas = pd.read_csv('')       # Por o caminho do arquivo
+
+abre_arquivo_csv_com_pandas                         # Para visualizar os primeiros no banco
