45047734297db51bbe4fb7b8cadfd99139e28405
Pedro Lucas <pedro.a.kruger@gmail.com>
2025-03-18 13:35:59 -0300

    Junção de funções organizações

--- PDFCollectorBackup.py
+++ PDFCollectorBackup.py
@@ -31,33 +31,42 @@ MarckRed = int(config.get('MarckRed', 0))
 
 reader = ocr.Reader(['pt','en'], model_storage_directory=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'ocr'))
 
-# Salva o arquivo na pasta temp e retorna o caminho do arquivo (temp_path)
+def create_folder_year(base_folder_path, year): 
+    base_folder_year = os.path.join(base_folder_path, f'Arquivo {year}')
+    if not os.path.exists(base_folder_year):
+        os.makedirs(base_folder_year)
+        log.info(f'Pasta criada: {base_folder_year}')
+        return base_folder_year
+    return base_folder_year
+
 def save_temp(attachment):
     file_extension = os.path.splitext(attachment.FileName)[1] 
-    '''
-    file_extension          -->     *ARMAZENA A EXTENÇÃO DO ARQUIVO ANEXO*
-    attachment.FileName     -->     Nome do anexo
-    os.path.splitext()      -->     Divide o nome do arquivo em nome e extenção 0 = Nome 1 = Extenção
-    [1]                     -->     Selecionamos a extenção
-    '''
     try:
         with tf.NamedTemporaryFile(delete=False, suffix=file_extension) as file_temp:
-            '''
-            Cria um arquivo temporario, não permite que seja apagado, define o sufixo como file_extension (extenção do anexo) e define os arquivos atraves da variavel file_temp
-            '''
             temp_path = file_temp.name
-            '''
-            temp_path       =       Caminho completo do arquivo na pasta temporaria
-            '''
             attachment.SaveAsFile(temp_path)
-            '''
-            Salva o arquivo na pasta temporaria
-            '''
         return temp_path
     except Exception as e:
         log.error(f'Erro ao criar arquivo temporário {attachment.FileName}: {e}')
+        return None
 
 def save_attachment(attachment, destination_folder_year, domain, receipt_date): # RETORNA UMA LISTA DE ARQUIVOS SALVOS - PODE APENAS FAZER ALGO, NÃO PRECISA RETORNAR
+    '''    
+def save_attachment(attachment, destination_folder_year, domain):
+    try:
+        destination_folder_domain = os.path.join(destination_folder_year, domain)
+        destination_attachment = os.path.join(destination_folder_domain, attachment.FileName)
+        if not os.path.exists(destination_folder_domain):
+            os.makedirs(destination_folder_domain)
+            log.info(f'Pasta criada: {destination_folder_domain}')
+        if not os.path.exists(destination_attachment):
+            attachment.SaveAsFile(destination_attachment)
+            log.info(f'Anexo salvo em: {destination_attachment}') 
+        return destination_attachment
+    except Exception as e:
+        log.error(f'Erro ao salvar o anexo {attachment.FileName}: {e}')
+        return None
+'''
     try:
         timestamp = receipt_date.strftime('%Y%m%d%H%M%S') 
         destination_folder_domain = os.path.join(destination_folder_year, domain)
@@ -85,8 +94,24 @@ def status_checkmark(item, status):
     except Exception as e:
         log.error(f'Erro ao marcar email {attachment.FileName}: {e}')
         return None
+    
+def extract_text_from_compact_file(file_path, poppler_path='poppler-24.08.0\\Library\\bin', dpi=300):       # Esta dentro de process_pdfs_compressed
+    pdf_img = p2i(file_path, dpi=dpi, poppler_path=poppler_path)
 
-def image_correction(image):                                                                                    # Usado diretamente no processamento da imagem e na verificação se pdf -> img (extract_text_from_compact_file)
+    text_content = []
+    
+    for img_convert in pdf_img:
+        image = cv2.cvtColor(np.array(img_convert), cv2.COLOR_RGB2BGR)
+        improved_img = correct_skew(image)
+        result_ocr = reader.readtext(improved_img)
+        
+        page_text = ' '.join([text[1] for text in result_ocr])
+        
+        if page_text:
+            text_content.append(page_text)
+    return text_content
+
+def correct_skew(image):                                                                                    # Esta dentro de extract_text_from_compact_file
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
     coords = np.column_stack(np.where(thresh > 0))
@@ -101,68 +126,74 @@ def image_correction(image):                                                    
     rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
     return rotated
 
+def clear_path(extract_path):
+    if os.path.exists(extract_path):
+        for item in os.listdir(extract_path):
+            item_path = os.path.join(extract_path, item)
+            try:
+                if os.path.isdir(item_path):
+                    shutil.rmtree(item_path)
+                else:
+                    os.remove(item_path)
+            except Exception as e:
+                print(f'Erro ao tentar remover {item_path}: {e}')
+
+def move_files_to_root(extract_path):
+    for root, _, files in os.walk(extract_path):
+        for file in files:
+            src = os.path.join(root, file)
+            dest = os.path.join(extract_path, file)
+            if src != dest:
+                shutil.move(src, dest)
+
 def extract_files(file_temp, ext):
     """ Extrai arquivos compactados para uma pasta temporária e retorna a lista de arquivos extraídos. """
     extract_path = os.path.join(tf.gettempdir(), f"extracted_{ext.strip('.')}")
     os.makedirs(extract_path, exist_ok=True)
+    
     try:
         if ext == '.zip':
             with zipfile.ZipFile(file_temp, 'r') as zip_ref:
                 zip_ref.extractall(path=extract_path)
                 file_paths = zip_ref.namelist()
+
         elif ext == '.7z':
             with py7zr.SevenZipFile(file_temp, 'r') as sevenz_ref:
                 sevenz_ref.extractall(path=extract_path)
                 file_paths = sevenz_ref.getnames()
+
         elif ext == '.rar':
             with rarfile.RarFile(file_temp, 'r') as rar_ref:
                 rar_ref.extractall(path=extract_path)
                 file_paths = rar_ref.namelist()
+
         # TESTAR - VERIFICAR SE ESTA FUNCIONAL
         elif ext in ('.tar', '.gz'):
             with tarfile.open(file_temp, 'r:*') as tar_ref:
                 tar_ref.extractall(path=extract_path)
                 file_paths = tar_ref.getnames()
                 #  gzip.GzipFile
-            # FALTA ACRESCENTAR
-        for root, _, files in os.walk(extract_path):
-            for file in files:
-                src = os.path.join(root, file)
-                dest = os.path.join(extract_path, file)
-                if src != dest:
-                    shutil.move(src, dest)
+
+        move_files_to_root(extract_path)
         return extract_path, file_paths
     except Exception as e:
         log.error(f"Erro ao extrair {ext}: {e}")
         return None, []
 
-def process_pdfs_compressed(extract_path, poppler_path='poppler-24.08.0\\Library\\bin', dpi=300):
+def process_pdfs_compressed(extract_path):                              # Script unico
     """ Processa arquivos PDF extraídos e extrai o texto. """
     text_content = []
     for file in os.listdir(extract_path):
         if file.lower().endswith('.pdf'):
             file_path = os.path.join(extract_path, file)
             try:
-                # Tentar extrair texto diretamente do PDF
                 with ppl.open(file_path) as pdf:
                     for page in pdf.pages:
                         text = page.extract_text()
                         if text and text.strip():
                             text_content.append(text)
-                # Se não houver texto extraído diretamente, usar OCR
                 if not text_content:
-                    # Converter o PDF em imagens
-                    pdf_img = p2i(file_path, dpi=dpi, poppler_path=poppler_path)
-                    for img_convert in pdf_img:
-                        # Corrigir possíveis distorções ou rotações na imagem
-                        image = cv2.cvtColor(np.array(img_convert), cv2.COLOR_RGB2BGR)
-                        improved_img = image_correction(image)  # Corrigir a imagem
-                        # Realizar OCR para extrair texto
-                        result_ocr = reader.readtext(improved_img)
-                        page_text = ' '.join([text[1] for text in result_ocr])
-                        if page_text:
-                            text_content.append(page_text)
-                # Log do progresso
+                    text_content = extract_text_from_compact_file(file_path)
                 log.info(f'Texto extraído do PDF {file}: {text_content[:100]}...')
             except Exception as e:
                 log.error(f"Erro ao processar PDF {file}: {e}")
@@ -187,13 +218,11 @@ try:
                     receipt_year = receipt_date.year
                     sender = 'pedro@bosch.com'
                     domain = sender.split('@')[1].split('.')[0] 
-                    if domain is not None:
-                        destination_folder_year = os.path.join(base_folder_path, f'Arquivo {receipt_year}')
-                        if not os.path.exists(destination_folder_year):
-                            os.makedirs(destination_folder_year)
-                            log.info(f'Pasta criada: {destination_folder_year}')
 
+                    if domain is not None:
+                        destination_folder_year = create_folder_year(base_folder_path, receipt_year)
                         for attachment in item.Attachments:
+
                             try:
                                 if attachment.FileName.lower().endswith(('.pdf')):
                                     file_temp = save_temp(attachment)
@@ -202,25 +231,36 @@ try:
                                             for page in pdf.pages:
                                                 text = page.extract_text()
                                                 if text and text.strip():
+                                                    
+                                                    
                                                     '''
+
                                                     LOGICA PARA IDENTIFICAÇÃO SE O CONTEUDO É CERTIFICADO
+                                                    
                                                     '''
                                                     #save_attachment(file_temp)
+
                                                     log.info(f'Texto extraído do PDF {attachment.FileName}: {text[:100]}...')
+
                                                 else:
                                                     # ACRESCENTAR TRY/EXCEPT
-                                                    pdf_img = p2i(file_temp, dpi=300, poppler_path='poppler-24.08.0\\Library\\bin')
-                                                    for i, img_convert in enumerate(pdf_img):
-                                                        image = cv2.cvtColor(np.array(img_convert), cv2.COLOR_RGB2BGR)
-                                                        improved_img = image_correction(image)
-                                                        result_ocr = reader.readtext(improved_img)
+                                                    pdf_img = p2i(file_temp, dpi=300, poppler_path='poppler-24.08.0\\Library\\bin')               # Uma lista de imagens, onde cada imagem representa uma página do PDF.
+                                                    for i, img_convert in enumerate(pdf_img):                                   # Itera sobre cada imagem e á aloca uma por vez em img_convert
+                                                        image = cv2.cvtColor(np.array(img_convert), cv2.COLOR_RGB2BGR)          # Converter imagem PIL para OpenCV               
+                                                        improved_img = correct_skew(image)                                   # Corrigir inclinação da imagem
+                                                        result_ocr = reader.readtext(improved_img)                           # Processar a imagem com OCR
+                                                    
                                                         '''
                                                         LOGICA PARA IDENTIFICAÇÃO SE O CONTEUDO É CERTIFICADO
                                                         '''
+
                                                     #save_attachment(file_temp)
+
                                                     log.info(f'Texto extraído do PDF/IMG {attachment.FileName}: {result_ocr[:100]}...')
+                                                                                                                            
                                     os.remove(file_temp)
                                     status_checkmark(item, MarckCheck)
+                           
                             except Exception as e:
                                 log.error(f'Erro ao processar arquivo PDF {attachment.FileName}: {e}')  
                             
@@ -231,13 +271,18 @@ try:
                                         result_ocr = reader.readtext(file_temp)
                                         if result_ocr:
                                             ...
+                                    
                                         '''
                                         LOGICA PARA IDENTIFICAÇÃO SE O CONTEUDO É CERTIFICADO                      
                                         '''
                                         # save_attachment()
+
                                         log.info(f'Texto extraído do JPG/PNG {attachment.FileName}: {result_ocr[:100]}...')
+                                        
+                                        
                                     os.remove(file_temp)
                                     status_checkmark(item, MarckCheck)        
+                               
                             except Exception as e:
                                 log.error(f'Erro ao processar arquivo de imagem {attachment.FileName}: {e}')
 
@@ -247,30 +292,28 @@ try:
                                     if file_temp:
                                         ext = os.path.splitext(file_temp)[1].lower()
                                         extract_path, file_paths = extract_files(file_temp, ext)
+
                                         if extract_path:
                                             text_content = process_pdfs_compressed(extract_path)
+                                            
                                             '''
                                             LOGICA PARA IDENTIFICAÇÃO SE O CONTEUDO É CERTIFICADO   
+
                                             USAR text_content (ONDE O TEXTO É RETORNADO)                   
                                             '''
                                             # save_attachment()
-                                            if os.path.exists(extract_path):
-                                                for item in os.listdir(extract_path):
-                                                    item_path = os.path.join(extract_path, item)
-                                                    try:
-                                                        if os.path.isdir(item_path):
-                                                            shutil.rmtree(item_path)
-                                                        else:
-                                                            os.remove(item_path)
-                                                    except Exception as e:
-                                                        print(f'Erro ao tentar remover {item_path}: {e}')
+
+                                            clear_path(extract_path)
+                                    
                                     os.remove(file_temp)        
-                                    status_checkmark(item, MarckCheck)                          
+                                    status_checkmark(item, MarckCheck)       
+                                                 
                             except Exception as e:
                                 log.error(f'Erro ao processar o arquivo compactado {attachment.FileName}: {e}')            
                 
                 except Exception as e:
                     log.error(f'Erro ao extrair informações do email {sender}: {e}') 
+
                     item.MarkAsTask(MarckRed)         
                     item.FlagStatus = MarckRed
                     item.Save()
